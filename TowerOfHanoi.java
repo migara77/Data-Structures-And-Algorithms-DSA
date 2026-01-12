3ced089310211afff00cf203f175dd0f1da5bf80
migara77 <136375588+migara77@users.noreply.github.com>
2026-01-12 19:18:17 +0530

    Add files via upload

---
+++ TowerOfHanoi.java
@@ -0,0 +1,21 @@
+
+package submission4;
+
+class TowerOfHanoi {
+
+    static void hanoi(int n, char source, char auxiliary, char destination) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+
+        hanoi(n - 1, source, destination, auxiliary);
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+        hanoi(n - 1, auxiliary, source, destination);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        hanoi(n, 'A', 'B', 'C');
+    }
+}
