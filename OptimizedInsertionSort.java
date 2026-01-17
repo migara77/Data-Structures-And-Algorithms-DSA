b9f9a0f1d984c0d461861e26a85f718d0addaa26
migara77 <136375588+migara77@users.noreply.github.com>
2026-01-17 17:43:05 +0530

    Add files via upload

---
+++ OptimizedInsertionSort.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package submission5;
+
+public class OptimizedInsertionSort {
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements greater than key to one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+
+        insertionSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
